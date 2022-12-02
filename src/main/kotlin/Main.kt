6791785d146b1a882345da6dda6b9b3d674223dd
Anthony Fernandes <anthonyf@phase2online.com>
2022-12-01 20:17:13 -0600

    init commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
+
+fun readFile(fileName: String): List<String> {
+    return File(fileName).useLines { it.toList() }
+}
